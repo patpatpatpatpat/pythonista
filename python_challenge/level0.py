ff32e70b9c9e90954765ed3f7b54abca8decf376
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-01-26 19:29:09 +0800

    I'll try to solve 1 level of Python challenge everyday.

---
+++ python_challenge/level0.py
@@ -0,0 +1,6 @@
+"""
+Level 0: http://www.pythonchallenge.com/pc/def/0.html
+
+Use exponentiation in Python.
+"""
+num = 2**38
---
+++ python_challenge/level1.py
@@ -0,0 +1,5 @@
+"""
+Level 1: http://www.pythonchallenge.com/pc/def/map.html
+
+Solution coming soon!
+"""
