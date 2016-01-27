cea54d17f72d183740f75e3e9c99c5acda831bce
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-01-27 09:40:33 +0800

    Print URL of next challenge when code is executed.

--- python_challenge/level0.py
+++ python_challenge/level0.py
@@ -4,3 +4,4 @@ Level 0: http://www.pythonchallenge.com/pc/def/0.html
 Use exponentiation in Python.
 """
 num = 2**38
+print 'http://www.pythonchallenge.com/pc/def/{}.html'.format(num)
--- python_challenge/level1.py
+++ python_challenge/level1.py
@@ -1,5 +1,19 @@
 """
 Level 1: http://www.pythonchallenge.com/pc/def/map.html
 
-Solution coming soon!
+Use string.maketrans and `translate` string method.
+
+K -> M
+O -> Q
+E - G
 """
+import string
+to = 'cdefghijklmnopqrstuvwxyzab'
+frm = 'abcdefghijklmnopqrstuvwxyz'
+
+trans_table = string.maketrans(frm, to)
+clue = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+print clue.translate(trans_table)
+
+next_url = 'map'.translate(trans_table)
+print 'http://www.pythonchallenge.com/pc/def/{}.html'.format(next_url)
