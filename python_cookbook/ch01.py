c4fbeed957d6020817b99cdb6bba861213b70102
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-04-08 14:49:40 +0800

    Python cookbook reading progress

---
+++ python_cookbook/ch01.py
@@ -0,0 +1,15 @@
+"""
+Chapter 1
+
+1. Use Python "star expressions" to unpack elements from iterables of arbitrary length.
+2. Use `collections.deque` to store the last few items during iteration or during some processing.
+3. Use `heapq` to get the list of largest or smallest N items in a collection
+4. Use `collections.defaultdict` and initialize the first value so you can focus on adding items.
+5. Use `collections.OrderedDict` when creating a dictionary where you can control the order of items when iterating
+6. Use `zip` to group items together
+7. You can use `key` argument when using `min` and `max` functions
+8. Use set operations to find out what two dicts (keys or values) have in common: &, -
+9. Use `set` and generators to remove duplicates from a sequence while maintaining order
+10. Use `slice` class to avoid mysterious hardcoded indices
+11. Use `collections.Counter` class when determining most frequently occuring items in a sequence. You can also use mathematical operations in instances of them
+"""
